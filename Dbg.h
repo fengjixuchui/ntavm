d072db58097dc30dab719281594c3da535aa579b
NTAurora <38770072+NTAurora@users.noreply.github.com>
2020-05-01 21:52:03 -0400

    Fixed DbgPrintElevated

--- Dbg.h
+++ Dbg.h
@@ -2,4 +2,4 @@
 #include "stdafx.h"
 VOID DbgkCreateThread(PETHREAD Thread, PVOID StartAddress);
 VOID DbgkExitThread(NTSTATUS ExitStatus);
-#define DbgPrintElevated(msg, var) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, msg, var);
+#define DbgPrintElevated(msg, ...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, msg, __VA_ARGS__)
